6344f4b22ddb12a3eafd3bde64af0964b2db011a
matteokeole <matteo@studiokg.fr>
2024-06-28 13:29:51 +0200

    wip: mat3x3/4x4 header files

---
+++ mat/mat3x3.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+template<typename T>
+struct mat3x3 {
+	private:
+		T entries[9];
+
+	public:
+		mat3x3();
+
+		mat3x3(const mat3x3&);
+
+		mat3x3(const mat3x3&&);
+
+		~mat3x3();
+
+		T operator [](int) const;
+
+		mat3x3& operator =(const mat3x3&);
+
+		mat3x3&& operator *(const matrix3&) const &;
+
+		mat3x3&& operator *(const matrix3&) &&;
+
+		mat3x3& operator *=(const matrix3&);
+
+		mat3x3& invert();
+
+		mat3x3& transpose();
+};
+
+template<typename T>
+inline std::ostream& operator <<(std::ostream&, const mat3x3<T>&);
---
+++ mat/mat4x4.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+template<typename T>
+struct mat4x4 {
+	private:
+		T entries[16];
+
+	public:
+		mat4x4();
+
+		mat4x4(const mat4x4&);
+
+		mat4x4(const mat4x4&&);
+
+		~mat4x4();
+
+		T operator [](int) const;
+
+		mat4x4& operator =(const mat4x4&);
+
+		mat4x4&& operator *(const matrix3&) const &;
+
+		mat4x4&& operator *(const matrix3&) &&;
+
+		mat4x4& operator *=(const matrix3&);
+
+		mat4x4& invert();
+
+		mat4x4& transpose();
+};
+
+template<typename T>
+inline std::ostream& operator <<(std::ostream&, const mat4x4<T>&);
